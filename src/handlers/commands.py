dbe92f02f815c15cef226356723cb8587e1a6bbe
Fedor <fedoravdeev3@gmail.com>
2024-11-21 04:42:17 +0700

    set auth pots

--- src/handlers/commands.py
+++ src/handlers/commands.py
@@ -1,7 +1,8 @@
 from aiogram import Router, F, types
 from aiogram.filters import Command, CommandStart
 from prisma.models import User
-from settings import my_keyboard_buttons, messages_no_profile, messages_help
+from settings import my_keyboard_buttons, messages_no_profile, messages_help, message_profile
+from helpers.user import getActiveUser
 
 router = Router()
 
@@ -24,31 +25,61 @@ async def command_start_handler(message: types.Message) -> None:
 
 @router.message(Command("channel"))
 async def command_channel_handler(message: types.Message) -> None:
-    await message.answer(messages_no_profile)
+    user_id = message.chat.id
+    is_user_active = await getActiveUser(user_id)
+    if is_user_active == True:
+        await message.answer(message_profile)
+    else:
+        await message.answer('Тут каталог')
 
 @router.message(Command("pay"))
 async def command_pay_handler(message: types.Message) -> None:
-    await message.answer(messages_no_profile)
+    user_id = message.chat.id
+    is_user_active = await getActiveUser(user_id)
+    if is_user_active == True:
+        await message.answer(message_profile)
+    else:
+        await message.answer('Тут плтёжка')
 
 @router.message(Command("newopt"))
 async def command_newopt_handler(message: types.Message) -> None:
-    await message.answer(messages_no_profile)
+    user_id = message.chat.id
+    is_user_active = await getActiveUser(user_id)
+    if is_user_active == True:
+        await message.answer(message_profile)
+    else:
+        await message.answer('Тут создание опта')
 
 @router.message(Command("getopt"))
 async def command_getopt_handler(message: types.Message) -> None:
-    await message.answer(messages_no_profile)
+    user_id = message.chat.id
+    is_user_active = await getActiveUser(user_id)
+    if is_user_active == True:
+        await message.answer(message_profile)
+    else:
+        await message.answer('Тут создание опта')
 
 @router.message(Command("business"))
 async def command_business_handler(message: types.Message) -> None:
-    await message.answer(messages_no_profile)
+    await message.answer('Тут подборки')
 
 @router.message(Command("profile"))
 async def command_profile_handler(message: types.Message) -> None:
-    await message.answer(messages_no_profile)
+    user_id = message.chat.id
+    is_user_active = await getActiveUser(user_id)
+    if is_user_active == True:
+        await message.answer(message_profile)
+    else:
+        await message.answer(message_profile)
 
 @router.message(Command("partners"))
 async def command_partners_handler(message: types.Message) -> None:
-    await message.answer(messages_no_profile)
+    user_id = message.chat.id
+    is_user_active = await getActiveUser(user_id)
+    if is_user_active == True:
+        await message.answer(message_profile)
+    else:
+        await message.answer('Тут промокоды')
 
 @router.message(Command("help"))
 async def command_help_handler(message: types.Message) -> None:
--- src/handlers/keyboard_buttons.py
+++ src/handlers/keyboard_buttons.py
@@ -1,17 +1,32 @@
 from aiogram import Router, F, types
 from aiogram.filters import Command, CommandStart
-from settings import my_keyboard_buttons, messages_no_profile, messages_help
+from settings import my_keyboard_buttons, messages_no_profile, messages_help, message_profile
+from helpers.user import getActiveUser
 
 router = Router()
 
 @router.message(F.text == 'Профиль')
-async def command_start_handler(message: types.Message) -> None:
-    await message.answer(messages_no_profile)
+async def command_profile_handler(message: types.Message) -> None:
+    user_id = message.chat.id
+    is_user_active = await getActiveUser(user_id)
+    if is_user_active == True:
+        await message.answer(message_profile)
+    else:
+        await message.answer(messages_no_profile)
 
 @router.message(F.text == 'Каталог')
-async def command_start_handler(message: types.Message) -> None:
-    await message.answer(messages_no_profile)
+async def command_catalog_handler(message: types.Message) -> None:
+    user_id = message.chat.id
+    is_user_active = await getActiveUser(user_id)
+    if is_user_active == True:
+        await message.answer("Тут каталог")
+    else:
+        await message.answer(messages_no_profile)
 
 @router.message(F.text == 'Помощь')
-async def command_start_handler(message: types.Message) -> None:
+async def command_help_handler(message: types.Message) -> None:
     await message.answer(messages_help)
+
+
+
+
