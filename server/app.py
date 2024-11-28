f9e6efd1d2ec49161d306776cac4d85a2bf8627d
Fedor <fedoravdeev3@gmail.com>
2024-11-28 14:45:39 +0700

    add create suggestions

--- server/app.py
+++ server/app.py
@@ -4,7 +4,7 @@ from flask import Flask, redirect, url_for
 from flask import render_template, request
 from flask_login import LoginManager
 
-from handlers.login import handler_login 
+from handlers.common import handler_login, handler_create_suggestions, handler_get_suggestions
 
 app = Flask(__name__)
 
@@ -28,9 +28,30 @@ def login_page():
 
     return render_template('login.html', message=message)
 
-@app.route('/', methods=['get'])
+@app.route('/', methods=['post', 'get'])
 def main_page():
-    return render_template('suggestions_create.html')
+    if request.method == 'POST':
+        username = request.form.get('username')
+        retail_price = request.form.get('retailPrice')
+        wholesale_price = request.form.get('wholesalePrice')
+        min_seats = request.form.get('minSeats')
+        max_seats = request.form.get('maxSeats')
+        details = request.form.get('details')
+        asyncio.run(handler_create_suggestions(
+            username=username,
+            retail_price=retail_price,
+            wholesale_price=wholesale_price,
+            min_seats=min_seats,
+            max_seats=max_seats,
+            details=details
+        ))
+
+    return render_template('suggestions_create.html', page="create")
+
+@app.route('/list', methods=['get'])
+def list_page():
+    suggestions = asyncio.run(handler_get_suggestions())
+    return render_template('suggestions_list.html', page="list", suggestions=suggestions)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3002)
---
+++ server/handlers/common.py
@@ -0,0 +1,42 @@
+from prisma import Prisma
+from prisma.models import Admin, Suggestions
+
+prisma = Prisma(auto_register=True)
+
+async def handler_login(username, password):
+    await prisma.connect()
+    admin = await Admin.prisma().find_first(
+        where={
+            'username': username,
+            'password': password,
+        },
+    )
+    await prisma.disconnect()
+    return admin
+
+async def handler_get_suggestions():
+    await prisma.connect()
+    suggestions = await Suggestions.prisma().find_many()
+    await prisma.disconnect()
+    return suggestions
+
+async def handler_create_suggestions(
+    username,
+    retail_price,
+    wholesale_price,
+    min_seats,
+    max_seats,
+    details
+):
+    await prisma.connect()
+    suggestions = await Suggestions.prisma().create(
+        data = {
+            'username': username,
+            'retail_price': retail_price,
+            'wholesale_price': wholesale_price,
+            'min_seats': min_seats,
+            'details': details
+        },
+    )
+    await prisma.disconnect()
+    return suggestions
