fbba5cb2b2e6778c8a5074104d50ed05b68bdf9d
Fedor <fedoravdeev3@gmail.com>
2024-12-02 14:52:47 +0700

    add table time

--- server/static/js/script.js
+++ server/static/js/script.js
@@ -1,7 +1,29 @@
+let datesSelect = []
+let timesSelect = []
+let inputDate
+
 document.addEventListener("DOMContentLoaded", function() {
+  inputDate = document.getElementById("input-date");
+  getDate()
+  getTime()
+});
+
+function getDate(page = 0) {
   const containerDate = document.getElementById("container-date");
 
-  const dates = getDate();
+  containerDate.innerHTML = "";
+
+  let today = new Date();
+
+  const dates = [];
+
+  today.setDate(today.getDate() + (page * 10));
+
+  for (let i = 0; i < 10; i++) {
+      let date = new Date(today);
+      date.setDate(today.getDate() + i);
+      dates.push(date.toISOString().split('T')[0]);
+  }
 
   const divDateTitle = document.createElement('div');
   const divMorningTitle = document.createElement('div');
@@ -34,11 +56,25 @@ document.addEventListener("DOMContentLoaded", function() {
     divDate.innerHTML = date;
 
     divMorning.className = "btn btn-primary";
-    divMorning.setAttribute('data-date', date);
+    divMorning.setAttribute('data-date', `${date}/УТРО`);
+    if(datesSelect.includes(`${date}/УТРО`)) {
+      divMorning.setAttribute('data-is-selected', 'active');
+      divMorning.innerHTML ="✅"
+    }
+
     divDay.className = "btn btn-primary";
-    divDay.setAttribute('data-date', date);
+    divDay.setAttribute('data-date', `${date}/ДЕНЬ`);
+    if(datesSelect.includes(`${date}/ДЕНЬ`)) {
+      divDay.setAttribute('data-is-selected', 'active');
+      divDay.innerHTML ="✅"
+    }
+
     divEvening.className = "btn btn-primary";
-    divEvening.setAttribute('data-date', date);
+    divEvening.setAttribute('data-date', `${date}/ВЕЧЕР`);
+    if(datesSelect.includes(`${date}/ВЕЧЕР`)) {
+      divEvening.setAttribute('data-is-selected', 'active');
+      divEvening.innerHTML ="✅"
+    }
 
     divMorning.addEventListener("click", setDate)
     divDay.addEventListener("click", setDate)
@@ -59,29 +95,28 @@ document.addEventListener("DOMContentLoaded", function() {
   divBack.className = "last-row btn btn-primary";
   divNext.className = "last-row btn btn-primary";
 
+  divBack.addEventListener("click", () => getDate(page - 1 <= 0? 0: page - 1))
+  divNext.addEventListener("click", () => getDate(page + 1))
+
   containerDate.appendChild(divBack)
   containerDate.appendChild(divNext)
-});
-
-function getDate() {
-  let today = new Date(page = 0);
-
-  today.setDate(today.getDate() + (page * 10));
-
-  let dates = [];
-
-  for (let i = 0; i < 10; i++) {
-      let date = new Date(today);
-      date.setDate(today.getDate() + i);
-      dates.push(date.toISOString().split('T')[0]);
-  }
-
-  console.log(dates);
-
-  return dates
 }
 
 function setDate(event) {
+  if(event.target.dataset.isSelected) {
+    event.target.dataset.isSelected = ""
+    event.target.innerHTML = ""
+    const inputDateValueArray = inputDate.value.split(" ").filter(date => date !== event.target.dataset.date)
+    inputDate.value = inputDateValueArray.join(" ")
+  } else {
+    event.target.dataset.isSelected = "active"
+    event.target.innerHTML = "✅"
+    inputDate.value = ((inputDate.value? inputDate.value: '') + ' ' + event.target.dataset.date).trim();
+  }
+  console.log("inputDate.value: ", inputDate.value)
+}
+
+function nextDate(event) {
   if(event.target.dataset.isSelected) {
     event.target.dataset.isSelected = ""
     event.target.innerHTML = ""
@@ -89,5 +124,54 @@ function setDate(event) {
     event.target.dataset.isSelected = "active"
     event.target.innerHTML = "✅"
   }
-  console.log("event: ", event)
+}
+
+function getTime() {
+  const containerTime = document.getElementById("container-time");
+
+  containerTime.innerHTML = "";
+
+  const times = ['8/10', '13/10', '18/10', '9/10', '14/10', '19/10', '10/10', '15/10', '20/10', '11/10', '16/10', '21/10', '12/10', '17/10', '22/10']
+
+  const divMorningTitle = document.createElement('div');
+  const divDayTitle = document.createElement('div');
+  const divEveningTitle = document.createElement('div');
+
+  divMorningTitle.innerHTML = "Утро";
+  divDayTitle.innerHTML = "День";
+  divEveningTitle.innerHTML = "Вечер";
+
+  divMorningTitle.className = "btn btn-primary";
+  divDayTitle.className = "btn btn-primary";
+  divEveningTitle.className = "btn btn-primary";
+
+
+  containerTime.appendChild(divMorningTitle)
+  containerTime.appendChild(divDayTitle)
+  containerTime.appendChild(divEveningTitle)
+
+  for(const time of times) {
+    const divTime = document.createElement('div');
+
+    divTime.className = "btn btn-primary";
+    divTime.innerHTML = time;
+
+    divTime.addEventListener("click", setTime)
+
+    containerTime.appendChild(divTime)
+  }
+}
+
+function setTime(event) {
+  if(event.target.dataset.isSelected) {
+    event.target.dataset.isSelected = ""
+    event.target.innerHTML = ""
+    const inputDateValueArray = inputDate.value.split(" ").filter(date => date !== event.target.dataset.date)
+    inputDate.value = inputDateValueArray.join(" ")
+  } else {
+    event.target.dataset.isSelected = "active"
+    event.target.innerHTML = "✅"
+    inputDate.value = ((inputDate.value? inputDate.value: '') + ' ' + event.target.dataset.date).trim();
+  }
+  console.log("inputDate.value: ", inputDate.value)
 }
